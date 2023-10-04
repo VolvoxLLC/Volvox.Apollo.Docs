207685eaa657d224eb720a874f5b7a0a4f9dead1
Ethan Guest <eguest01@gmail.com>
2023-10-04 07:33:27 -0500

    add docs proj

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Apollo.Volvox.Docs</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.gg/Y6BgvsWuNU',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
