e84721afe807df2b13251a27561b55c98b6911ee
Bill Chirico <bill@chirico.dev>
2023-10-27 11:38:13 -0400

    Upgrade to NextJS 14 and fix config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs"
+import React from "react"
 
 const config: DocsThemeConfig = {
-  logo: <span>Apollo.Volvox.Docs</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.gg/Y6BgvsWuNU',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+	logo: <span>Volvox.Apollo Docs</span>,
+	project: {
+		link: "https://github.com/VolvoxLLC/Volvox.Apollo.Docs"
+	},
+	chat: {
+		link: "https://discord.gg/Y6BgvsWuNU"
+	},
+	docsRepositoryBase: "https://github.com/VolvoxLLC/Volvox.Apollo.Docs",
+	footer: {
+		text: "Volvox.Apollo Docs © 2023 by Volvox LLC"
+	}
 }
 
 export default config
