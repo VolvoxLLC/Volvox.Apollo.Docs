2e3f704fd810959b29ab3e20f85b69eda6a17564
Bill Chirico <bill@chirico.dev>
2023-11-07 11:55:11 -0500

    feat(Plugins): Add docusaurus-plugin-sass
    Added the `docusaurus-plugin-sass` plugin to the list of custom plugins in the Docusaurus configuration file. This plugin allows for Sass support in the project.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -29,6 +29,9 @@ const config: Config = {
     locales: ['en'],
   },
 
+  // Custom Plugins
+  plugins: ['docusaurus-plugin-sass'],
+
   presets: [
     [
       'classic',
@@ -48,15 +51,14 @@ const config: Config = {
             'https://github.com/VolvoxLLC/Volvox.Apollo.Docs/tree/master',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: ['./src/css/custom.scss']
         },
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/Apollo-Banner.jpg',
     navbar: {
       title: 'Volvox.Apollo Docs',
       logo: {
@@ -77,14 +79,14 @@ const config: Config = {
           label: 'Docs'
         },
         {
-          to: '/blog', 
-          label: 'Blog', 
+          to: '/blog',
+          label: 'Blog',
           position: 'left'
         },
         {
           to: 'https://github.com/VolvoxLLC/Volvox.Apollo.Docs',
-          label: 'GitHub',
           position: 'right',
+          className: 'github-link',
         },
       ],
     },
@@ -105,11 +107,11 @@ const config: Config = {
           items: [
             {
               label: 'Discord',
-              href: 'https://discord.gg/Y6BgvsWuNU',
+              to: 'https://discord.gg/Y6BgvsWuNU',
             },
             {
               label: 'Twitter',
-              href: 'https://twitter.com/volvoxllc',
+              to: 'https://twitter.com/volvoxllc',
             },
           ],
         },
@@ -121,19 +123,26 @@ const config: Config = {
               to: '/blog',
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/VolvoxLLC/Volvox.Apollo.Docs',
+              to: 'https://github.com/VolvoxLLC/Volvox.Apollo.Docs',
+              className: 'github-link',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Volvox LLC`,
+      copyright: `Copyright © ${new Date().getFullYear()} Volvox LLC<br />
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Licensed by Volvox LLC under a Creative Commons Attribution-ShareAlike 4.0 International License</a><br /><br />
+      <span class="designedBy">Designed with <svg xmlns="http://www.w3.org/2000/svg" class="heart" width="24" height="24" viewBox="0 0 24 24"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"/></svg>
+      by <a href="https://github.com/VolvoxLLC">Volvox</a></span>`,
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  plugins: [
+    'docusaurus-plugin-sass'
+  ]
 };
 
 export default config;
