c5c900d77d61af9ec026553df59f0e02144f2e3b
Bill Chirico <bill@chirico.dev>
2023-11-07 12:32:00 -0500

    Update config and add Twitter Logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -29,9 +29,6 @@ const config: Config = {
     locales: ['en'],
   },
 
-  // Custom Plugins
-  plugins: ['docusaurus-plugin-sass'],
-
   presets: [
     [
       'classic',
@@ -67,10 +64,9 @@ const config: Config = {
       },
       items: [
         {
-          to: 'https://apollo.volvox.tech',
-          label: 'Home',
           position: 'left',
-          target: '_parent'
+          label: 'Home',
+          to: '/',
         },
         {
           type: 'doc',
@@ -84,9 +80,14 @@ const config: Config = {
           position: 'left'
         },
         {
-          to: 'https://github.com/VolvoxLLC/Volvox.Apollo.Docs',
+          to: 'https://github.com/VolvoxLLC',
           position: 'right',
           className: 'github-link',
+        },
+        {
+          to: 'https://discord.gg/Y6BgvsWuNU',
+          position: 'right',
+          className: 'discord-link',
         },
       ],
     },
@@ -108,10 +109,12 @@ const config: Config = {
             {
               label: 'Discord',
               to: 'https://discord.gg/Y6BgvsWuNU',
+              className: 'discord-link',
             },
             {
               label: 'Twitter',
               to: 'https://twitter.com/volvoxllc',
+              className: 'twitter-link',
             },
           ],
         },
@@ -123,14 +126,15 @@ const config: Config = {
               to: '/blog',
             },
             {
-              to: 'https://github.com/VolvoxLLC/Volvox.Apollo.Docs',
+              label: ' ',
+              to: 'https://github.com/VolvoxLLC',
               className: 'github-link',
             },
           ],
         },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Volvox LLC<br />
-      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Licensed by Volvox LLC under a Creative Commons Attribution-ShareAlike 4.0 International License</a><br /><br />
+      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Licensed by Volvox LLC under a Creative Commons Attribution-ShareAlike 4.0 International License</a><br />
       <span class="designedBy">Designed with <svg xmlns="http://www.w3.org/2000/svg" class="heart" width="24" height="24" viewBox="0 0 24 24"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z"/></svg>
       by <a href="https://github.com/VolvoxLLC">Volvox</a></span>`,
     },
@@ -140,6 +144,7 @@ const config: Config = {
     },
   } satisfies Preset.ThemeConfig,
 
+  // Custom Plugins
   plugins: [
     'docusaurus-plugin-sass'
   ]
