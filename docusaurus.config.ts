93fd1e7bc95df4f5f92f38162adb920456cff686
Bill Chirico <bill@chirico.dev>
2023-11-07 14:16:03 -0500

    feat(Docs): Update link to intro page
    The commit updates the link to the intro page in the navigation menu of the documentation. The previous link '/apollo-docs/intro' has been changed to '/docs/intro'. This change ensures that users are directed to the correct location when clicking on the 'Intro' label in the navigation menu.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,7 +104,7 @@ const config: Config = {
           items: [
             {
               label: 'Intro',
-              to: '/apollo-docs/intro',
+              to: '/docs/intro',
             },
           ],
         },
