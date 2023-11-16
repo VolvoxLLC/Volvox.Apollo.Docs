3676826f98d3e326a7bce1a850a81b5991fcfdca
Bill Chirico <bill@chirico.dev>
2023-11-15 21:36:44 -0500

    feat(Search): Disable contextual search
    This commit updates the `docusaurus.config.ts` file to disable contextual search by setting the `contextualSearch` property to `false`. This change was made to improve the user experience and prevent unintended navigation when using Algolia search.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -65,7 +65,7 @@ const config: Config = {
       indexName: 'apollo-volvox',
 
       // Optional: see doc section below
-      contextualSearch: true,
+      contextualSearch: false,
 
       // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
       externalUrlRegex: 'apollo.volvox\\.tech',
